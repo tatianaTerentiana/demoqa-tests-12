3664d8726cdde5c36aad6a3619845486aa0b7ec4
Tatiana Terentyeva <tterentieva17@gmail.com>
2022-04-12 16:26:16 +0300

    added small tests

--- src/test/java/guru/qa/JUnitTests.java
+++ src/test/java/guru/qa/JUnitTests.java
@@ -1,10 +1,8 @@
 package guru.qa;
 
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.*;
 
-public class SimpleTest {
+public class JUnitTests {
 
     @BeforeAll
     static void initDB() {
